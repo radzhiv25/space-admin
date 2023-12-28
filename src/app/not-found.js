2c78dee787f87f08b456b903a5532583dadb00a2
radzhiv <radzhivkrishna@gmail.com>
2023-12-28 20:08:24 +0530

    feat: not-found, globals and page updated

---
+++ src/app/not-found.js
@@ -0,0 +1,8 @@
+export default function notFound(){
+    return (
+        <div className="top-1/2 left-1/2 absolute transform -translate-x-1/2">
+            <h1 className="text-lg font-semibold">Not found 👀</h1>
+            <p className="text-xs">thing you are looking for are not available</p>
+        </div>
+    )
+}
