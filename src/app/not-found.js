651f434ab1460f74844b96acbb48a910e726021f
radzhiv <radzhivkrishna@gmail.com>
2023-12-29 00:09:37 +0530

    feat: minor change

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -2,7 +2,7 @@ export default function notFound(){
     return (
         <div className="top-1/2 left-1/2 absolute transform -translate-x-1/2">
             <h1 className="text-lg font-semibold">Not found 👀</h1>
-            <p className="text-xs">thing you are looking for are not available</p>
+            <p className="text-xs">thing you are looking for is not available</p>
         </div>
     )
 }
