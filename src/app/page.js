4fc05b4e347ebbd17ec86f7274e9bbf45db58933
radzhiv <radzhivkrishna@gmail.com>
2023-12-29 13:48:26 +0530

    feat: next-auth added

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,15 @@
 import Link from 'next/link'
 import Hero from './components/hero';
+import Footer from './components/footer';
+import Navbar from './components/navbar';
 
 export default function Home() {
   return (
     <div className="w-screen">
       {/* <h1>hey 👋🏻</h1> */}
-      <div className="m-5 p-3 flex justify-between items-center border-b">
-        <div className="leading-tight">
-          <h1 className="md:text-xl text-lg font-semibold">Space admin</h1>
-          <p className="text-xs text-gray-400">A place to see space happenings</p>
-        </div>
-        <div className="text-gray-500 font-sans space-x-5">
-          <Link href='/signup' className='hover:text-black'>Sign Up</Link>
-          <Link href='/login' className='hover:text-gray-700'>Login</Link>
-        </div>
-      </div>
+      <Navbar />
       <Hero />
+      <Footer />
     </div>
   );
 }
